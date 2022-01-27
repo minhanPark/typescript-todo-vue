98bb2a87912e6c143a0c939cf4127b8dd1f51ae0
Running water <alsgks1218@naver.com>
2022-01-27 23:46:00 +0900

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <template>
-  <div class="container">
-    <span class="header">타입스크립트 투두</span>
-    <h1 class="header">ㅎㅔ더부분</h1>
-  </div>
+  <Header />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Header from "./components/Header.vue";
 
 export default defineComponent({
   name: "App",
+  components: {
+    Header,
+  },
 });
 </script>
 
-<style lang="scss">
-$color: black;
-$font-color: white;
-.container {
-  background-color: $color;
-}
-.header {
-  color: $font-color;
-}
-</style>
+<style lang="scss"></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>Header</div>
+  <div class="container">
+    <h1 class="text-wrapper">Typescript Todos</h1>
+  </div>
 </template>
 
 <script>
-export default {};
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.container {
+  max-width: 500px;
+  width: 100%;
+  background-color: #f3a683;
+  color: white;
+  padding: 8px 10px;
+  border: 2px solid #e77f67;
+  margin: 30px auto;
+}
+.text-wrapper {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
