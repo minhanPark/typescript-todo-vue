dd6170d4c6a2843c96f82606d0526d92cc4b7f15
Running water <alsgks1218@naver.com>
2022-01-28 00:56:47 +0900

    add setTodo, deleteTodo, addTodo func

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Header />
+  <TodosContainer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "./components/Header.vue";
+import TodosContainer from "./components/TodosContainer.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Header,
+    TodosContainer,
   },
 });
 </script>
--- src/components/TodosContainer.vue
+++ src/components/TodosContainer.vue
@@ -1,9 +1,76 @@
 <template>
-  <div>TodoContainer</div>
+  <div class="container"></div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface ITodo {
+  id: number;
+  title: string;
+  desc: string;
+  isCompleted: boolean;
+}
+
+export default defineComponent({
+  name: "TodosContainer.vue",
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Make todo",
+          desc: "make todo with me",
+          isCompleted: false,
+        },
+        {
+          id: 2,
+          title: "sleep early",
+          desc: "please Sleep early asap",
+          isCompleted: false,
+        },
+        {
+          id: 3,
+          title: "game",
+          desc: "Play game with friend",
+          isCompleted: true,
+        },
+      ] as ITodo[],
+    };
+  },
+  methods: {
+    setTodos(newTodos: ITodo[]): void {
+      this.todos = newTodos;
+    },
+    // 삭제기능
+    deleteTodo(id: number): void {
+      const newArr = this.todos.filter((todo) => todo.id !== id);
+      this.setTodos(newArr);
+    },
+    // Complete수정기능
+    // 추가 기능
+    addTodo({ title, desc }: { title: string; desc: string }): void {
+      const id = this.todos[this.todos.length - 1]["id"] + 1;
+      this.setTodos([
+        ...this.todos,
+        {
+          id,
+          title,
+          desc,
+          isCompleted: false,
+        },
+      ]);
+    },
+  },
+});
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.container {
+  max-width: 450px;
+  width: 100%;
+  border: 2px solid #f3a683;
+  margin: 0 auto;
+  min-height: 500px;
+}
+</style>
